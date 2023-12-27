b29752688268edd74dffd90fe230c758324d3e2d
MLDODSON <paola@dominicansoul.com>
2023-12-26 19:30:58 -0600

    Branch 15
    Branch 15  - Updated the App.jsx and CocktailCard.jsx files.

--- mixx-master/src/components/CockTailCard.jsx
+++ mixx-master/src/components/CockTailCard.jsx
@@ -1,6 +1,20 @@
-const CockTailCard = () => {
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
+
+const CockTailCard = ({ id, name, image, info, glass }) => {
+
     return (
-        <h2>CockTail Card</h2>
+        <Wrapper>
+            <div className="img-container">
+                <img src={image} alt={name} className="img" />
+            </div>
+            <div className="footer">
+                <h4>{name}</h4>
+                <h5>{glass}</h5>
+                <p>{info}</p>
+                <Link to={`/cocktail/${id}`} className="btn">Details</Link>
+            </div>
+        </Wrapper>
     )
 }
 
