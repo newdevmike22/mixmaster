1ada173cb467c574612170e9749caa8279edf253
MLDODSON <paola@dominicansoul.com>
2023-12-29 09:54:12 -0600

    Branch 22
    Branch 22 - Set up the search form's functionality and styled it.

--- mixx-master/src/components/SearchForm.jsx
+++ mixx-master/src/components/SearchForm.jsx
@@ -1,6 +1,26 @@
-const SearchForm = () => {
+import Wrapper from "../assets/wrappers/SearchForm";
+import { Form, useNavigation } from "react-router-dom";
+
+const SearchForm = ({searchTerm}) => {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+
     return (
-        <h2>Search Form</h2>
+        <Wrapper>
+            <Form className="form">
+                <input 
+                    type="search" 
+                    name="search" 
+                    className="form-input" 
+                    defaultValue={searchTerm} 
+                />
+                <button 
+                    type="submit" 
+                    className="btn" 
+                    disabled={isSubmitting}>{isSubmitting?'searching...' : 'search'}
+                </button>
+            </Form>
+        </Wrapper>
     )
 }
 
